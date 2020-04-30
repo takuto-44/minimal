99cbda9f9a64f8ee1aa8255f71ebb9c55b12623d
takuto-44 <59508944+takuto-44@users.noreply.github.com>
2020-05-01 03:08:50 +0900

    fix_deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock '3.12.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'MINIMAL'
+set :application, 'minimal'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:takuto-44/minimal.git'
