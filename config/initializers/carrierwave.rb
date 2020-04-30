1bc474103e44a40f9ebdb1ac0b638e880afa8533
takuto-44 <59508944+takuto-44@users.noreply.github.com>
2020-05-01 01:27:43 +0900

    fix_carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,15 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
+  config.storage = :file
+  config.file_provider = 'file/aws'
+  config.file_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.credentials[:aws][:access_key_id],
     aws_secret_access_key: Rails.application.credentials[:aws][:secret_access_key],
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'minimal'
+  config.file_directory  = 'minimal'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/minimal'
 end
